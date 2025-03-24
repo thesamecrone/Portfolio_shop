f3cd3dd7f5820c9900ee119d2abef0f5c4088294
NATALIA VOITEKHOVICH <thesamecrone@gmail.com>
2025-03-24 20:47:23 +0300

    return

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -3,6 +3,20 @@ document.getElementById("search-icon").addEventListener("click", function () {
     document.getElementById("search-form").classList.toggle("show");
 });
 
+//search input trial
+const searchInput = document.getElementById("search-input");
+document.getElementById("search-form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Отменяем стандартное поведение формы
+
+    const query = searchInput.value.trim().toLowerCase();
+    if (query) {
+        // Перенаправляем на страницу shop с параметром в URL
+        window.location.href = `html/7.Shop.html?search=${encodeURIComponent(query)}`;
+    }
+});
+
+
+//end
 document.getElementById("cart-icon").addEventListener("click", function () {
     var cartContent = document.getElementById("cart-content");
     cartContent.style.display = (cartContent.style.display === "none") ? "block" : "none";
