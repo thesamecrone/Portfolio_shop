d2949f6ea47e08b1834b1b05fc785b42d232c8af
NATALIA VOITEKHOVICH <thesamecrone@gmail.com>
2025-10-20 15:50:06 +0300

    responsive pages done

--- bikeshop/js/bike5.js
+++ bikeshop/js/bike5.js
@@ -22,3 +22,44 @@ const navMenu = document.querySelector('.nav-menu');
 menuToggle.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
+
+// carousel
+
+document.addEventListener('DOMContentLoaded', function() {
+  const carousel = document.querySelector('.album');
+  const arrowLeft = document.querySelector('.arrow1');
+  const arrowRight = document.querySelector('.arrow2');
+  const cards = document.querySelectorAll('.card');
+
+  if (!carousel || !arrowLeft || !arrowRight || cards.length === 0) {
+    console.error('Elements were not found!');
+    return;
+  }
+
+  cards.forEach(card => {
+    const clone = card.cloneNode(true);
+    carousel.appendChild(clone);
+  });
+
+  const totalCards = cards.length;
+  const cardWidth = cards[0].offsetWidth + 63;
+  let currentPosition = 0;
+
+  arrowLeft.addEventListener('click', function() {
+    currentPosition = (currentPosition - 1 + totalCards) % totalCards;
+    const scrollToPosition = currentPosition * cardWidth;
+    carousel.scrollTo({
+      left: scrollToPosition,
+      behavior: 'smooth'
+    });
+  });
+
+  arrowRight.addEventListener('click', function() {
+    currentPosition = (currentPosition + 1) % totalCards;
+    const scrollToPosition = currentPosition * cardWidth;
+    carousel.scrollTo({
+      left: scrollToPosition,
+      behavior: 'smooth'
+    });
+  });
+});
