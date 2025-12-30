bc7ff9a02978d10a8c2a3f25a9894ca06f53106d
NATALIA VOITEKHOVICH <thesamecrone@gmail.com>
2025-12-30 14:31:15 +0300

    modal close added

--- bikeshop/js/common.js
+++ bikeshop/js/common.js
@@ -1,18 +1,18 @@
 // translation
 
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement(
-        { pageLanguage: 'en', includedLanguages: 'en,ru' },
-        'google_translate_element'
-    );
+  new google.translate.TranslateElement(
+    { pageLanguage: 'en', includedLanguages: 'en,ru' },
+    'google_translate_element'
+  );
 }
 
 function setLang(lang) {
-    const select = document.querySelector("select.goog-te-combo");
-    if (select) {
-        select.value = lang;
-        select.dispatchEvent(new Event("change"));
-    }
+  const select = document.querySelector("select.goog-te-combo");
+  if (select) {
+    select.value = lang;
+    select.dispatchEvent(new Event("change"));
+  }
 }
 
 let currentLang = "en";
@@ -25,21 +25,21 @@ const flagEN = "https://thesamecrone.github.io/Portfolio_shop/bikeshop/images/Ma
 const flagRU = "https://upload.wikimedia.org/wikipedia/en/f/f3/Flag_of_Russia.svg";
 
 langButton.addEventListener("click", () => {
-    if (currentLang === "en") {
-        setLang("ru");
-        currentLang = "ru";
-        langText.textContent = "RU";
-        langImg.src = flagRU;
-        langImg.style.width = "14px";
-        langImg.style.height = "18px";
-    } else {
-        setLang("en");
-        currentLang = "en";
-        langText.textContent = "EN";
-        langImg.src = flagEN;
-        langImg.style.width = "14px";
-        langImg.style.height = "18px";
-    }
+  if (currentLang === "en") {
+    setLang("ru");
+    currentLang = "ru";
+    langText.textContent = "RU";
+    langImg.src = flagRU;
+    langImg.style.width = "14px";
+    langImg.style.height = "18px";
+  } else {
+    setLang("en");
+    currentLang = "en";
+    langText.textContent = "EN";
+    langImg.src = flagEN;
+    langImg.style.width = "14px";
+    langImg.style.height = "18px";
+  }
 });
 
 // modal window 
@@ -49,15 +49,15 @@ const modal = document.getElementById("loginModal");
 const closeModal = document.getElementById("closeModal");
 
 loginBtn.addEventListener("click", () => {
-    modal.style.display = "flex";
+  modal.style.display = "flex";
 });
 
 closeModal.addEventListener("click", () => {
-    modal.style.display = "none";
+  modal.style.display = "none";
 });
 
 window.addEventListener("click", (e) => {
-    if (e.target === modal) modal.style.display = "none";
+  if (e.target === modal) modal.style.display = "none";
 });
 
 // scrollToTop
@@ -71,7 +71,7 @@ scroll.addEventListener('click', () => {
 });
 
 window.addEventListener('scroll', () => {
-  if(window.scrollY === 0) {
+  if (window.scrollY === 0) {
     scroll.style.visibility = 'hidden';
   } else {
     scroll.style.visibility = 'visible';
@@ -94,58 +94,58 @@ const form = document.querySelector('.field');
 const emailInput = document.querySelector('.entermail');
 
 form.addEventListener('submit', async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const email = emailInput.value.trim();
-    if(!email) {
-        alert('Please enter your email');
-        return;
+  const email = emailInput.value.trim();
+  if (!email) {
+    alert('Please enter your email');
+    return;
+  }
+
+  try {
+    const res = await fetch('https://velorettibackend-production.up.railway.app/api/subscribe', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+      credentials: 'include'
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      alert("Subscribed successfully");
+      emailInput.value = "";
+    } else {
+      alert(data.error || "Subscription failed");
     }
+  }
 
-    try {
-        const res = await fetch('https://velorettibackend-production.up.railway.app/api/subscribe', {
-            method: 'POST',
-            headers: {
-                "Content-Type" : "application/json",
-            },
-            body: JSON.stringify({ email }),
-            credentials: 'include'
-        });
-
-        const data = await res.json();
-
-        if(res.ok) {
-            alert("Subscribed successfully");
-            emailInput.value = "";
-        } else {
-            alert(data.error || "Subscription failed");
-        }
-    } 
-
-    catch (err) {
-        console.error(err);
-        alert("Something went wrong");
-    }
+  catch (err) {
+    console.error(err);
+    alert("Something went wrong");
+  }
 });
 
 // user check
 
 async function checkUser() {
-    try {
-        const res = await fetch('https://velorettibackend-production.up.railway.app/api/me', {
-            credentials: 'include'
-        });
-        if(res.ok) {
-            const data = await res.json();
-            console.log('Logged in user:', data.user);
-            const userDisplay = document.querySelector('.user-display');
-            if(userDisplay) {
-                userDisplay.textContent = `Hello, ${data.user.name}`;
-            }
-        }
-    } catch (err) {
-        console.error('Error fetching user:', err);
+  try {
+    const res = await fetch('https://velorettibackend-production.up.railway.app/api/me', {
+      credentials: 'include'
+    });
+    if (res.ok) {
+      const data = await res.json();
+      console.log('Logged in user:', data.user);
+      const userDisplay = document.querySelector('.user-display');
+      if (userDisplay) {
+        userDisplay.textContent = `Hello, ${data.user.name}`;
+      }
     }
+  } catch (err) {
+    console.error('Error fetching user:', err);
+  }
 }
 
 checkUser();
@@ -184,6 +184,7 @@ function showToast(message) {
 window.addEventListener('message', (event) => {
   if (event.data?.type === 'GOOGLE_AUTH_SUCCESS') {
     const user = event.data.user;
+    modal.style.display = 'none';
 
     showToast(`Authenticated as ${user.name}`);
   }
